f46f42bdfb9c1fc0dc6fcf2db04f18fbc553818e
jjcindy115 <108634945+jjcindy115@users.noreply.github.com>
2022-07-05 01:30:15 +0500

    Add files via upload

---
+++ untitled.sh
@@ -0,0 +1,24 @@
+paru -S awesome-git
+paru -Sy picom-git wezterm rofi acpi acpid acpi_call upower lxappearance-gtk3 \
+jq inotify-tools polkit-gnome xdotool xclip gpick ffmpeg blueman redshift \
+pipewire pipewire-alsa pipewire-pulse pamixer brightnessctl feh scrot \
+mpv mpd mpc mpdris2 python-mutagen ncmpcpp playerctl --needed
+systemctl --user enable mpd.service
+systemctl --user start mpd.service
+git clone --recurse-submodules https://github.com/rxyhn/dotfiles.git
+cd dotfiles && git submodule update --remote --merge
+cp -r config/* ~/.config/
+cp -r misc/fonts/* ~/.fonts/
+# or to ~/.local/share/fonts
+cp -r misc/fonts/* ~/.local/share/fonts/
+fc-cache -v
+sudo cp -rf misc/themes/Aesthetic-Night/* /usr/share/themes
+cp -rf misc/themes/Aesthetic-Night-GTK4/* ~/.config/gtk-4.0
+Section "InputClass"
+    Identifier "touchpad"
+    Driver "libinput"
+    MatchIsTouchpad "on"
+    Option "Tapping" "on"
+    Option "TappingButtonMap" "lmr"
+    Option "NaturalScrolling" "true"
+EndSection
