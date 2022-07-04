7e8a92a3cdb37a68ceac51ab8e05c29cb6611386
jjcindy115 <108634945+jjcindy115@users.noreply.github.com>
2022-07-05 02:41:55 +0500

    Add files via upload

---
+++ untitle.sh
@@ -0,0 +1,3 @@
+ $ paru -S --needed bspwm sxhkd rofi neovim alacritty viewnior picom-arian8j2-git brightnessctl \
+           playerctl hsetroot maim jq xclip imagemagick dunst i3lock-color xdo giph \ 
+           jgmenu redshift
