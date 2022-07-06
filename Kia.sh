60a1bb90d5d6b47a0384e516b1d87614f9821bfe
jjcindy115 <108634945+jjcindy115@users.noreply.github.com>
2022-07-07 01:00:47 +0500

    Create Kia.sh

---
+++ Kia.sh
@@ -0,0 +1,2 @@
+yay -S git neovim nautilus zafiro-icon-theme rofi alacritty rxfetch dunst appimagelauncher zsh fish picom-animations-git glava starship zsh-autosuggestions zsh-completions zsh-syntax-highlighting lxappearance polybar bspwm sxhkd nitrogen qt5ct nerd-fonts-source-code-pro nerd-fonts-jetbrains-mono
+
